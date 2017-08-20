fd37eb44d7bf9995c8209b65bad0ff31e2b1b1bf
Camille <camille@pc47.home>
2017-08-20 17:56:49 +0200

    tiles move

--- components/Board.js
+++ components/Board.js
@@ -3,18 +3,60 @@ import { StyleSheet, Text, View } from 'react-native'
 import { width } from '../helper'
 import Tile from './Tile'
 
+const side = width / 3
+
+
 export default class Board extends React.Component {
-  render() {
+  constructor() {
+     super()
+     this.state = {
+       letters: Array(9).fill(null),
+     }
+
+     this.state.letters= "keyboard".split('')
+   }
+   updateBoard(pos, direction) {
+    const letters = this.state.letters.slice()
+    const letter = letters[pos]
+    letters[pos] = null
+    let newpos
+    switch (direction) {
+      case 'up':
+      newpos = pos - 3
+      break
+      case 'down':
+      newpos = pos + 3
+      break
+      case 'left':
+      newpos = pos - 1
+      break
+      case 'right':
+      newpos = pos + 1
+      break
+    }
+    if (!letters[newpos] && newpos >= 0 && newpos < 9) {
+      letters[newpos] = letter
+       this.setState({letters: letters})
+    }
+   }
+   renderTile(i) {
+     return(<Tile
+       letter={this.state.letters[i]}
+       onMove={(dir) => {this.updateBoard(i,dir)} }>
+       </Tile>)
+     }
+     render() {
     return (
       <View style={styles.container}>
-        <Tile letter='K'> </Tile>
-        <Tile letter='E'> </Tile>
-        <Tile letter='Y'> </Tile>
-        <Tile letter='B'> </Tile>
-        <Tile letter='O'> </Tile>
-        <Tile letter='A'> </Tile>
-        <Tile letter='R'> </Tile>
-        <Tile letter='D'> </Tile>
+        <View style={styles.cell}>{this.renderTile(0)}</View>
+        <View style={styles.cell}>{this.renderTile(1)}</View>
+        <View style={styles.cell}>{this.renderTile(2)}</View>
+        <View style={styles.cell}>{this.renderTile(3)}</View>
+        <View style={styles.cell}>{this.renderTile(4)}</View>
+        <View style={styles.cell}>{this.renderTile(5)}</View>
+        <View style={styles.cell}>{this.renderTile(6)}</View>
+        <View style={styles.cell}>{this.renderTile(7)}</View>
+        <View style={styles.cell}>{this.renderTile(8)}</View>
       </View>
     )
   }
@@ -30,5 +72,11 @@ const styles = StyleSheet.create({
     alignContent: 'space-between',
   //  justifyContent: 'space-around',
     flexWrap: 'wrap'
+  },
+  cell: {
+    height: side,
+    width: side,
+    backgroundColor: '#1915e9',
+    alignItems: 'center'
   }
 });
--- components/Tile.js
+++ components/Tile.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import GestureRecognizer, { swipeDirections } from 'react-native-swipe-gestures'
 import { width } from '../helper'
 
 const side = width / 3
-
+//ok
 export default class Tile extends React.Component {
+  move(direction) {
+    if (this.props.letter) {
+      this.props.onMove(direction)
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.props.letter}</Text>
-      </View>
-    )
+      <GestureRecognizer
+      onSwipeUp={() => this.move('up')}
+      onSwipeDown={() => this.move('down')}
+      onSwipeLeft={() => this.move('left')}
+      onSwipeRight={() => this.move('right')}>
+    <View style={this.props.letter ? styles.container: styles.none}>
+    <Text>{this.props.letter}</Text>
+    </View>
+    </GestureRecognizer>
+  )
   }
 }
 
@@ -23,5 +35,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#e9c115',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  none: {
+    height: side,
+    width: side,
+    borderWidth:1,
+    borderColor: '#3696b4',
+    backgroundColor: '#3696b4'
   }
 });
